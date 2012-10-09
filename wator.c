d8474dda9478bb609aca8190c73cdbacd95405ee
JJBug <james.mcr@hotmail.com>
2012-10-09 13:18:33 -0400

    new .c file

---
+++ wator.c
@@ -0,0 +1,89 @@
+/* Hello World program */
+
+#include<stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+main()
+{
+	srand(time(NULL));
+
+	int mapSize = 22;
+	int sharkNum = 10;
+	int fishNum = 20;		
+
+	char sharks[mapSize][mapSize];
+	char fish[mapSize][mapSize];
+	
+	// fill all arrays with ' '
+	int i;
+	for(i = 0; i < mapSize; i++) {
+		int i2;
+		for(i2 = 0; i2 < mapSize; i2++) {
+			sharks[i][i2] = ' ';
+			fish[i][i2] = ' ';
+
+			if ( i2 == mapSize ) {
+				i++;
+				i2 = 0;			
+			}
+		}
+	}
+	
+	// Create sharks and add them to array
+	int j;
+	for(j = 0; j < sharkNum; j++) {
+		int r = rand() % mapSize;
+		int r2 = rand() % mapSize;
+		sharks[r][r2] = '*'; 
+	}
+
+	// Create fish and add them to array
+	int f;
+	for(f = 0; f < fishNum; ) {
+		int r = rand() % mapSize;
+		int r2 = rand() % mapSize;
+		
+		// Create fish if generated index is empty
+		if(sharks[r][r2] == ' ' && fish[r][r2] == ' '){
+			fish[r][r2] = '.';
+			f++;
+		}
+	}
+	
+	int k;
+	for(k = 0; k < mapSize;) {
+		int k2;
+		for(k2 = 0; k2 < mapSize; k2++) {
+
+			// Print sharks
+			printf("%c", sharks[k][k2]);
+			
+			// Print fish
+			if ( fish[k][k2] != ' ') {
+				printf("%c", fish[k][k2]);
+			}
+
+			
+		}	
+
+		if ( k2 == mapSize ) {
+			printf("\n");
+			k++;
+			k2 = 0;
+		}
+	}
+
+}
+
+/*void createSharks(int pNum, int pSize, char pArray[][]) {
+	
+	int i;
+	for(i = 0; i < pNum; i++) {
+		int r = rand() % 20;
+		int r2 = rand() % 20;
+		pArray[r][r2] = '*'; 
+	}
+}*/
+
+
